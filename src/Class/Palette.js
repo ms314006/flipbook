b65a6b889248cc426456694bc3b7738d3e3cf558
GQSM <ms314006@hotmail.com>
2021-02-21 17:12:57 +0800

    add: finished palette block

---
+++ src/Class/Palette.js
@@ -0,0 +1,19 @@
+class Colors {
+  constructor() {
+    this.colors = [
+      '#ffffff00', '#ffffff',
+      '#E7C09D', '#c5a487',
+      '#8B5C33', '#5E2C00',
+      '#FD7FFF', '#EF0033',
+      '#FF6633', '#FFB500',
+      '#EEFF00', '#99EE00',
+      '#33CC00', '#00CFFC',
+      '#006FCF', '#004b8c',
+      '#4F00DF', '#A900DA',
+      '#000000', '#222222',
+      '#666666', '#AAAAAA',
+    ];
+  }
+}
+
+export default Colors;
--- src/Page/Main/index.tsx
+++ src/Page/Main/index.tsx
@@ -1,15 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Palette from '../../Class/Palette';
 import styles from './index.scss';
 
-const Main = () => (
-  <div className={styles.layout}>
-    <div className={styles.main}>
-      <div className={styles.colors}>Colors</div>
-      <div className={styles.workspace}>Workspace</div>
-      <div className={styles.showFlipbook}>Show Flipbook</div>
-      <div className={styles.pages}>Pages</div>
+const Main = () => {
+  const palette = new Palette();
+  const [currentColor, setCurrentColor] = useState(palette.colors[0]);
+
+  return (
+    <div className={styles.layout}>
+      <div className={styles.main}>
+        <div className={styles.actionBtns}>
+          <button
+            type="button"
+            className={styles.actionBtn}
+            onClick={() => { console.log('new'); }}
+          >
+            new
+          </button>
+          <button
+            type="button"
+            className={styles.actionBtn}
+            onClick={() => { console.log('new'); }}
+          >
+            add
+          </button>
+          <button
+            type="button"
+            className={styles.actionBtn}
+            onClick={() => { console.log('new'); }}
+          >
+            delete
+          </button>
+          <button
+            type="button"
+            className={styles.actionBtn}
+            onClick={() => { console.log('new'); }}
+          >
+            copy
+          </button>
+          <button
+            type="button"
+            className={styles.actionBtn}
+            onClick={() => { console.log('new'); }}
+          >
+            paste
+          </button>
+        </div>
+        <div className={styles.colors}>
+          {
+            palette.colors.map(color => (
+              <div
+                key={color}
+                className={`${styles.colorWapper} ${color === currentColor ? styles.focusColorWapper : ''}`}
+              >
+                <div
+                  role="button"
+                  tabIndex={0}
+                  className={`${styles.colorButton} ${color === currentColor ? styles.focusColorButton : ''}`}
+                  style={{ background: color }}
+                  onClick={() => { setCurrentColor(color); }}
+                  onKeyDown={() => { setCurrentColor(color); }}
+                />
+              </div>
+            ))
+          }
+        </div>
+        <div className={styles.workspace}>Workspace</div>
+        <div className={styles.showFlipbook}>Show Flipbook</div>
+        <div className={styles.pages}>Pages</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Main;
