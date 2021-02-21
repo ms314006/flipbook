3e4f2f2e6b126c1c08878e1e1d328a98c41697c0
GQSM <ms314006@hotmail.com>
2021-02-21 15:56:35 +0800

    add: finished main layout

--- src/Page/Main/index.scss.d.ts
+++ src/Page/Main/index.scss.d.ts
@@ -1,1 +1,6 @@
+export const layout: string;
 export const main: string;
+export const colors: string;
+export const workspace: string;
+export const showFlipbook: string;
+export const pages: string;
--- src/Page/Main/index.tsx
+++ src/Page/Main/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styles from './index.scss';
 
 const Main = () => (
-  <div className={styles.main}>
-    Main
+  <div className={styles.layout}>
+    <div className={styles.main}>
+      <div className={styles.colors}>Colors</div>
+      <div className={styles.workspace}>Workspace</div>
+      <div className={styles.showFlipbook}>Show Flipbook</div>
+      <div className={styles.pages}>Pages</div>
+    </div>
   </div>
 );
 
